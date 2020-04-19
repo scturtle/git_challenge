43a0d866fee1815a8c248ef6961d7c27f6efb76b
shenchao <shenchao@megvii.com>
2020-04-19 15:45:11 +0800

    fix checking

--- check.py
+++ check.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import sys
-from subprocess import check_output
+from subprocess import check_output, STDOUT
 
 def sh(cmd):
-    return check_output(cmd, shell=True).strip().decode()
+    return check_output(cmd, shell=True, stderr=STDOUT).strip().decode()
 
 def get_branch():
     if is_bisecting():
@@ -11,9 +11,10 @@ def get_branch():
     branch = sh('git rev-parse --abbrev-ref HEAD')
     if branch.endswith('_test'):
         branch = branch[:-5]
+    return branch
 
 def is_bisecting():
-    return sh('git bisect log') != 'We are not bisecting.'
+    return sh('git bisect log; echo') not in ['We are not bisecting.', '我们没有在二分查找。']
 
 def commit_and_messages():
     return [l.split(' ', 1) for l in sh("git log --abbrev-commit --pretty=format:'%h %s'").split('\n')]
@@ -21,7 +22,7 @@ def commit_and_messages():
 def file_content(fn='file'):
     return open(fn).read().strip()
 
-def git_diff():
+def git_dirty():
     return [l for l in sh("git diff HEAD").split('\n')if l and l[0] in '-+']
 
 def git_diff_between(older, newer):
@@ -31,18 +32,18 @@ def check_pull_diff():
     messages = [m for _, m in commit_and_messages()]
     assert messages == ['d', 'c', 'b', 'a'], messages
     content = file_content()
-    assert content == 'a\nb\nc\nd', repr(content)
-    assert git_diff() == []
+    assert content == 'c\na\nb\nd', repr(content)
+    assert git_dirty() == []
 
 def init_pull_dirty():
-    open('file', 'w').write('a\nb\nc\nd')
+    open('file', 'w').write('a\nb\nd')
 
 def check_pull_dirty():
     messages = [m for _, m in commit_and_messages()]
     assert messages == ['c', 'b', 'a'], messages
     content = file_content()
-    assert content == 'a\nb\nc\nd', repr(content)
-    diff = git_diff()
+    assert content == 'c\na\nb\nd', repr(content)
+    diff = git_dirty()
     assert diff == ['--- a/file', '+++ b/file', '+d'], diff
 
 def check_pick():
@@ -59,10 +60,11 @@ def check_squash_and_reorder():
     assert diff_de_bc == ['--- a/file', '+++ b/file', '+b', '+c'], diff_de_bc
 
 def check_bisect():
-    assert git_diff() == []
+    assert git_dirty() == []
     content = file_content()
     if content != '3751248':
         print('This commit is', 'good!' if int(content) < 3751248 else 'bad!')
+        assert False
     else:
         print('You found bad guy!')
 
